81dec7b9d8f151a8b131f9e2e7960bf0a31a4394
Vincent-Boateng <vincentboateng@gmail.com>
2020-11-25 12:03:31 -0500

    Added a new comment

--- src/Group8FirstCollaboration.java
+++ src/Group8FirstCollaboration.java
@@ -6,5 +6,7 @@ public class Group8FirstCollaboration {
         System.out.println("Hello Group 8!");
 
         //I will send an invitation to everyone
+
+        //I need this comment to show up in Github
     }
 }
