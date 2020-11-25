d595c1d71166676f54cb149b97ae031379814d3c
Vincent-Boateng <vincentboateng@gmail.com>
2020-11-25 12:01:17 -0500

    Initial commit

---
+++ src/Group8FirstCollaboration.java
@@ -0,0 +1,10 @@
+public class Group8FirstCollaboration {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello Group 8!");
+
+        //I will send an invitation to everyone
+    }
+}
