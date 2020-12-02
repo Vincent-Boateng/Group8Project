af69eccbf4d255dfac889cf0962e4cc9b4f99d5a
MVK-USA <mvk2015@yahoo.com>
2020-12-02 11:14:55 -0500

    initial commit

---
+++ src/Mariia/Class2.java
@@ -0,0 +1,9 @@
+package Mariia;
+
+public class Class2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Group-8!" +
+                "Have a nice day");
+    }
+}
